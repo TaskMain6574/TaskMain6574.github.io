5cc69cd635103541f9c80d9ba3e796497d27d127
Bren00x <Bren00x3@gmail.com>
2016-09-30 23:31:02 -0400

    Update

---
+++ assets/js/popup.js
@@ -0,0 +1,67 @@
+$( "#pop1" ).click(function() {
+$( "#popup1" ).toggle();
+});
+
+$( "#pop1" ).click(function() {
+$( ".pagecontent" ).toggle();
+});
+
+$( "#pop1close" ).click(function() {
+$( ".pagecontent" ).toggle();
+});
+
+$( "#pop1close" ).click(function() {
+$( "#popup1" ).toggle();
+});
+
+$( "#pop2" ).click(function() {
+$( "#popup2" ).toggle();
+});
+
+$( "#pop2" ).click(function() {
+$( ".pagecontent" ).toggle();
+});
+
+$( "#pop2close" ).click(function() {
+$( ".pagecontent" ).toggle();
+});
+
+$( "#pop2close" ).click(function() {
+$( "#popup2" ).toggle();
+});
+
+
+
+$( "#pop3" ).click(function() {
+$( "#popup3" ).toggle();
+});
+
+$( "#pop3" ).click(function() {
+$( ".pagecontent" ).toggle();
+});
+
+$( "#pop3close" ).click(function() {
+$( ".pagecontent" ).toggle();
+});
+
+$( "#pop3close" ).click(function() {
+$( "#popup3" ).toggle();
+});
+
+
+
+$( "#pop4" ).click(function() {
+$( "#popup4" ).toggle();
+});
+
+$( "#pop4" ).click(function() {
+$( ".pagecontent" ).toggle();
+});
+
+$( "#pop4close" ).click(function() {
+$( ".pagecontent" ).toggle();
+});
+
+$( "#pop4close" ).click(function() {
+$( "#popup4" ).toggle();
+});
